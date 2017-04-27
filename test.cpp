2c477743c2731ba09349bcfa43c488095a2b38b5
Izkare <jack@Izkare.com>
2017-04-27 15:53:03 +0800

    just for testing 3

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*just for test*/
+/*Git is free software distributed under the GPL.*/
+/*just add something diffrently in this area*/
+/*Git tracks changes.*/
+/*Git tracks changes of files.*/
+/*Creating a new branch is quick.*/
+/*Creating a new branch is quick and simple.*/
+#include <iostream>
+using namespace std;
+int main (int argc , char **argv)
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
